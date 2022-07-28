41d44a7a46041f812b0df387bf5112e5ab33bdb7
StavrosPlatis <104563009+StavrosPlatis@users.noreply.github.com>
2022-07-28 14:02:28 +0300

    Revert "Update hardhat.config.js"

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,5 @@
-require("dotenv").config()
-require("@nomiclabs/hardhat-etherscan")
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("solidity-coverage")
-require("hardhat-deploy")
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -17,7 +13,5 @@ module.exports = {
             chainId: 4,
         },
     },
-    solidity: {
-        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
-    },
+    solidity: "0.8.8",
 }
